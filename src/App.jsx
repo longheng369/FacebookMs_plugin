4ff20bfab07e41197a4a1a1e46183fa32b4b0edf
longheng369 <longhengchhoeun@gmail.com>
2024-05-15 00:03:38 +0700

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import FacebookMs from "./components/FacebookMs";
+// import FacebookMs from "./components/FacebookMs";
+import MessengerCustomerChat from 'react-messenger-customer-chat';
 function App() {
   return (
     <>
       <h1>hello</h1>
-      <FacebookMs />
+      {/* <FacebookMs /> */}
+      <MessengerCustomerChat
+        pageId="427377029894331"
+        appId="141916528997399"
+        htmlRef="my-messenger-plugin"
+      />
     </>
   );
 }
